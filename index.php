a9af91897aae97a51ce504395b8c530d782397a8
Udit Raj <udit.r@simformsolutions.com>
2023-02-21 15:29:26 +0530

    commit new

--- index.php
+++ index.php
@@ -1,6 +1,5 @@
 <?php
-echo "milan you need to make some changes.";
-echo "My wifi password is :'123456'";
+echo "milan you need to make some changes."
 # hello world, this is a comment.
 
 i made some new changes.
