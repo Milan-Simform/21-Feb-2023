b2006d7dd3963136506e179f0246a6cf16d2043f
Milan-Simform <milan.c@simformsolutions.com>
2023-02-21 15:30:45 +0530

    :sparkles: inserted wifi password in second line

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
-echo "milan you need to make some changes."
+echo "milan you need to make some changes.";
+echo "My wifi password is :'123456'";
 # hello world, this is a comment.
 
 i made some new changes.
